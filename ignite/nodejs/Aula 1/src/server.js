4aba2ba1c16e5ee660081ca39de3b956c95c62a5
JRebertt <joaorebertt0219@gmail.com>
2023-09-22 20:58:09 -0300

    add: method persist, and created class to db local in file

--- ignite/nodejs/Aula 1/src/server.js
+++ ignite/nodejs/Aula 1/src/server.js
@@ -1,23 +1,29 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
+import { Database } from './database.js'
 
-const users = []
+const database = new Database()
 
-const server = http.createServer((req, res) => {
+
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
+  await json(req, res)
 
   if (method === 'GET' && url === '/users') {
-    return res
-      .setHeader('Content-type', 'application/json')
-      .end(JSON.stringify(users))
+    const users = database.select('users')
+    return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
-    users.push({
+    const { name, email } = req.body
+    const user = {
       id: 1,
-      name: 'Jonh Doe',
-      email: 'jonhdoe@exemple.com'
-    })
+      name,
+      email
+    }
+
+    database.insert('users', user)
 
     return res.writeHead(201).end()
   }
